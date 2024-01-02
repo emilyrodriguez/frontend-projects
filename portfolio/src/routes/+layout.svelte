f80a8229c31ce78590faf4487346e1c8b9dd216d
Emily Rodriguez <rodriguez.emily1993@gmail.com>
2024-01-02 16:50:10 -0600

    Updating components

--- portfolio/src/routes/+layout.svelte
+++ portfolio/src/routes/+layout.svelte
@@ -2,4 +2,9 @@
     import "../app.css";
   </script>
   
+  <div class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen">
+    <div class="">
+        <button><i class="fa-solid fs-arrow-up"/></button>
+    </div>
+  </div>
   <slot />
