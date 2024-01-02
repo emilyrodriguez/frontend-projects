e03a1acdecbb250c23053555331dd9e4608295ad
Emily Rodriguez <rodriguez.emily1993@gmail.com>
2024-01-01 21:18:02 -0600

    Added logic to goFight() to display monster info

--- JavaScript_freecodecamp/RPG/script.js
+++ JavaScript_freecodecamp/RPG/script.js
@@ -148,7 +148,11 @@ function fightDragon() {
 }
 
 function goFight() {
-
+    update(locations[3]);
+    monsterHealth = monsters[fighting].health;
+    monsterStats.style.display = "block";
+    monsterName.innerText = monsters[fighting].name;
+    monsterHealthText.innerText = monsters[fighting].health;
 }
 
 function attack() {
