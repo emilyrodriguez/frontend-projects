6cfcc0fb07cef6d8c9d2b9c982f69afb34bb7464
Emily Rodriguez <rodriguez.emily1993@gmail.com>
2024-01-02 22:12:25 -0600

    Created addEntry() to dynamically create html

--- JavaScript_freecodecamp/CalorieCounter/script.js
+++ JavaScript_freecodecamp/CalorieCounter/script.js
@@ -8,6 +8,28 @@ const output = document.getElementById("output");
 let isError = false;
 
 function cleanInputString(str) {
-   const regex = /[+-\s]/g;
-   return str.replace(regex, "");
+	const regex = /[+-\s]/g;
+	return str.replace(regex, "");
 }
+
+function isInvalidInput(str) {
+	const regex = /\d+e\d+/i;
+	return str.match(regex);
+}
+
+function addEntry() {
+	const targetInputContainer = document.querySelector(
+		`#${entryDropdown.value} .input-container`
+	);
+	const entryNumber =
+		targetInputContainer.querySelectorAll('input[type="text"]').length;
+	const HTMLString = `
+        \n<label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+        \n<input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name"/>
+        \n<label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+        \n<input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories"/>`;
+
+    targetInputContainer.innerHTML += HTMLString;
+}
+
+addEntryButton.addEventListener("click", addEntry);
